7ecc1bb4489d14730775980c3d01ee519fbe4f9b
jscarlson <jcarlson238@gmail.com>
2021-12-29 17:19:32 -0500

    debug

--- datasets/coco.py
+++ datasets/coco.py
@@ -176,6 +176,7 @@ def build(image_set, args):
         "val": (root / "generated", root / "input" / f'train80_fixed.json'),
     }
     for k, v in PATHS.items():
+        print(v[0])
         assert os.path.isfile(v[0]) 
 
     img_folder, ann_file = PATHS[image_set]

2840fb1687e0cc8ae7f128b275a5e84f05c81cce
jscarlson <jcarlson238@gmail.com>
2021-12-29 17:17:48 -0500

    debug

--- datasets/coco.py
+++ datasets/coco.py
@@ -5,6 +5,7 @@ COCO dataset which returns image_id for evaluation.
 Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
 """
 from pathlib import Path
+import os
 
 import torch
 import torch.utils.data
@@ -172,8 +173,10 @@ def build(image_set, args):
     mode = 'instances'
     PATHS = {
         "train": (root / "generated", root / "input" / f'train80_fixed.json'),
-        "val": (root / "generated", root / "input" / f'test20_fixed.json'),
+        "val": (root / "generated", root / "input" / f'train80_fixed.json'),
     }
+    for k, v in PATHS.items():
+        assert os.path.isfile(v) 
 
     img_folder, ann_file = PATHS[image_set]
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set, args), return_masks=False)

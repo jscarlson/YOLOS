808d04a5503d4006432c6e50e9c999051a4271ad
jscarlson <jcarlson238@gmail.com>
2021-12-30 01:45:34 -0500

    debug

--- cocoval_predjson_generation.py
+++ cocoval_predjson_generation.py
@@ -180,6 +180,8 @@ def get_args_parser():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--use_checkpoint', action='store_true',
+                        help='use checkpoint.checkpoint to save mem')
     return parser
 
 
